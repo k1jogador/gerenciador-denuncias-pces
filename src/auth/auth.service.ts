b58b65c94d0d34f473015fdf97090e12d6389da8
k1jogador <78239211+k1jogador@users.noreply.github.com>
2025-10-20 11:51:05 -0300

    junção de senha incorreta com usuario nao existente

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,27 +1,25 @@
-import { JwtService } from '@nestjs/jwt';
-import { DatabaseService } from './../database/database.service';
+import { JwtService } from "@nestjs/jwt";
+import { DatabaseService } from "./../database/database.service";
 import {
   Injectable,
   NotFoundException,
   UnauthorizedException,
-} from '@nestjs/common';
+} from "@nestjs/common";
 
 @Injectable()
 export class AuthService {
   constructor(
     private databaseService: DatabaseService,
-    private jwtService: JwtService,
+    private jwtService: JwtService
   ) {}
 
   async signIn(matricula: string, senha: string): Promise<any> {
-    const user =
-      await this.databaseService.buscarUsuarioPorMatricula(matricula);
+    const user = await this.databaseService.buscarUsuarioPorMatricula(
+      matricula
+    );
 
-    if (!user) {
-      throw new NotFoundException('Registro não encontrado');
-    }
-    if (user.senha_hash !== senha) {
-      throw new UnauthorizedException('Credenciais inválidas');
+    if ((user.senha_hash !== senha) | !user) {
+      throw new UnauthorizedException("Credenciais inválidas");
     }
     const result = {
       sub: user.id,
