c2738e9f62c8605bc614d3379c9793b65abeadf7
Kauã <kauabernardes001@gmail.com>
2025-10-26 17:53:38 -0300

    atualizacao retorno da funcao signIn

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,38 +1,37 @@
-import { JwtService } from "@nestjs/jwt";
-import { DatabaseService } from "./../database/database.service";
+import { JwtService } from '@nestjs/jwt';
+import { DatabaseService } from './../database/database.service';
 import {
   Injectable,
   NotFoundException,
   UnauthorizedException,
-} from "@nestjs/common";
+} from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
   constructor(
     private databaseService: DatabaseService,
-    private jwtService: JwtService
+    private jwtService: JwtService,
   ) {}
 
-  async signIn(matricula: string, senha: string): Promise<any> {
-    const user = await this.databaseService.buscarUsuarioPorMatricula(
-      matricula
-    );
+  async signIn(
+    matricula: string,
+    senha: string,
+  ): Promise<{ access_token: string }> {
+    const user =
+      await this.databaseService.buscarUsuarioPorMatricula(matricula);
 
     if (!user) {
-      throw new UnauthorizedException("Não autorizado");
+      throw new UnauthorizedException('Não autorizado');
     }
     if (user.senha_hash !== senha) {
-      throw new UnauthorizedException("Não autorizado");
+      throw new UnauthorizedException('Não autorizado');
     }
-    const result = {
+    const payload = {
       sub: user.id,
-      nome: user.nome,
-      email: user.email,
-      matricula: user.matricula,
       id_perfil: user.id_perfil,
     };
     return {
-      access_token: await this.jwtService.signAsync(result),
+      access_token: await this.jwtService.signAsync(payload),
     };
   }
 }
